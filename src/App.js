1d626621237db4fe33b2e2f47a2ad54bb138d3f6
jayamiko <jayamiko4@gmail.com>
2024-06-12 15:41:06 +0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './style.css';
 import Card from './components/Card';
 import Pagination from './components/Pagination';
-import { products } from './data/data';
+import DataTable from './components/DataTable';
 
 export const App = () => {
   return (
     <div className="container">
       <Card />
       <Pagination />
+
+      <br />
+      <br />
+      <br />
+
+      {/* DATATABLE */}
+      <DataTable />
     </div>
   );
 };
---
+++ src/components/DataTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DataTable = () => {
+  return (
+    <div>
+      <i>Create a table component here!</i>
+    </div>
+  );
+};
+
+export default DataTable;

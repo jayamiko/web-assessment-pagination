abd5e96175ba4fa64d856117aec15d3f24d52157
jayamiko <jayamiko4@gmail.com>
2024-06-12 07:45:22 +0700

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './style.css';
 import Card from './components/Card';
 import Pagination from './components/Pagination';
+import { products } from './data/data.js';
 
 export const App = () => {
   return (
     <div className="container">
-      <Card />
+      {products.map((x) => {
+        return <Card />;
+      })}
       <Pagination />
     </div>
   );
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Macbook Pro M1 2020 Touchbar',
+    release_date: '10-10-2020',
+    OS: 'Mac',
+    Brand: 'Apple',
+    description:
+      'Apple memperkenalkan MacBook Pro 13 inci dengan dua port Thunderbolt berdasarkan sistem Apple M1 pada sebuah chip, diluncurkan bersamaan dengan MacBook Air dan Mac Mini yang diperbarui sebagai Mac pertama dengan jajaran produk baru berbasis ARM khusus Apple.',
+    image: {
+      url: 'https://asset.kompas.com/crops/ELqQOvrDHnZimhcDDdzjGDkmHV4=/215x133:1348x888/750x500/data/photo/2021/01/18/6004ff2047534.jpg',
+    },
+  },
+  {
+    id: 1,
+    name: 'Macbook Pro M1 2020 Touchbar',
+    release_date: '10-10-2020',
+    OS: 'Mac',
+    Brand: 'Apple',
+    description:
+      'Apple memperkenalkan MacBook Pro 13 inci dengan dua port Thunderbolt berdasarkan sistem Apple M1 pada sebuah chip, diluncurkan bersamaan dengan MacBook Air dan Mac Mini yang diperbarui sebagai Mac pertama dengan jajaran produk baru berbasis ARM khusus Apple.',
+    image: {
+      url: 'https://asset.kompas.com/crops/ELqQOvrDHnZimhcDDdzjGDkmHV4=/215x133:1348x888/750x500/data/photo/2021/01/18/6004ff2047534.jpg',
+    },
+  },
+];

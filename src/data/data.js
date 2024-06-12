3fcaaae83fa3815f100a1297668d818d18fe0569
jayamiko <jayamiko4@gmail.com>
2024-06-12 15:27:15 +0700

    1 file modified

--- src/data/data.js
+++ src/data/data.js
@@ -35,7 +35,7 @@ export const products = [
     name: 'Macbook Pro M1 2020 Touchbar',
     sold: 12,
     stock: 5,
-    storage: '128 GB',
+    storage: 128,
     brand_id: 1,
     price: '14.999.000',
     description:
@@ -49,7 +49,7 @@ export const products = [
     name: 'ASUS ROG Zephyrus G14 Ryzen 5',
     sold: 2,
     stock: 48,
-    storage: '128 GB',
+    storage: 128,
     brand_id: 4,
     price: '23.999.000',
     description:
@@ -63,7 +63,7 @@ export const products = [
     name: 'Lenovo Ideapad Slim 3i',
     sold: 80,
     stock: 102,
-    storage: '128 GB',
+    storage: 128,
     brand_id: 2,
     price: '5.400.000',
     description:
@@ -77,7 +77,7 @@ export const products = [
     name: 'ASUS VivoBook Go 14',
     sold: 66,
     stock: 44,
-    storage: '256 GB',
+    storage: 256,
     brand_id: 4,
     price: '6.299.000',
     description:
@@ -91,13 +91,41 @@ export const products = [
     name: 'ASUS Vivobook 14X M1403',
     sold: 10,
     stock: 0,
-    storage: '512 GB',
+    storage: 512,
     brand_id: 4,
     price: '9.999.000',
     description:
       'Vivobook 14X hadir dengan webcam beresolusi HD 720p. Webcam ini menggunakan teknologi ASUS 3D Noise Reduction (3DNR) yang diklaim dapat meningkatkan kejernihan secara signifikan dan mengurangi noise pada gambar. Kamera ini juga dilengkapi dengan webcam shield fisik untuk menjaga privasi pengguna.',
     image: {
       url: 'https://www.yangcanggih.com/wp-content/uploads/2023/02/ASUS-Vivobook-14X-2.jpg',
+    },
+  },
+  {
+    id: 6,
+    name: 'Macbook Pro M2 2022',
+    sold: 50,
+    stock: 50,
+    storage: 256,
+    brand_id: 1,
+    price: '17.999.000',
+    description:
+      'MacBook Pro 13 inci lebih andal dari sebelumnya. Bertenaga super berkat chip M2 generasi berikutnya, MacBook Pro ini merupakan laptop pro Apple yang paling portabel, dengan kekuatan baterai hingga 20 jam',
+    image: {
+      url: 'https://selular.id/wp-content/uploads/2022/07/macbook-pro-696x392-1.jpg',
+    },
+  },
+  {
+    id: 7,
+    name: 'Axioo Pongo Studio',
+    sold: 200,
+    stock: 175,
+    storage: 64,
+    brand_id: 3,
+    price: '15.699.000',
+    description:
+      'Axioo secara resmi menghadirkan laptop gaming terbarunya yakni Pongo Studio. Ini menjadi laptop paling kencang dalam lini produk Axioo Pongo berkat spesifikasi yang diberikan yakni Intel Core i9-13900H dan NVIDIA GeForce RTX 4070. Tak cuma itu, Axioo Pongo Studio ini juga menjadi laptop lokal pertama sudah mendapatkan sertifikasi NVIDIA Studio.',
+    image: {
+      url: 'https://selular.id/wp-content/uploads/2022/07/macbook-pro-696x392-1.jpg',
     },
   },
 ];
